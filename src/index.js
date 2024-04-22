1b022f7ced1c4d94f483f0b5cf6e09ac7f5c7b93
Ahmed-ELassa1 <ahmedhamed2150@gmail.com>
2024-04-22 22:16:40 +0200

    #fix: private to false in pack.json

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { UserProvider } from "./contexts/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-      <UserProvider>
-        <App />
-      </UserProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <UserProvider>
+    <App />
+  </UserProvider>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
